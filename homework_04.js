5ca2d7b34e8f5346486af734c1565ee58c6de1e0
AdrMel <melilloadriano@outlook.it>
2015-11-20 15:16:21 +0100

    dec2bin

--- homework_04.js
+++ homework_04.js
@@ -1,9 +1,40 @@
+function Stack() {
+    this.array = [];
+}
+
+// inserisce l'elemento in coda all'array
+Stack.prototype.push = function(e){
+	this.array[this.array.length] = e;
+};
+
+// inserisce l'elemento al penultimo posto dell'array
+Stack.prototype.pop = function(){
+	var temp = this.array[this.array.length - 1];
+	this.array.splice(this.array.length - 1, 1);
+	return temp;
+};
+
+// guarda l'elemento alla penultima posizione
+Stack.prototype.peek = function(){
+	return this.array[this.array.length - 1];
+};
+
+// controlla se l'array è vuoto
+Stack.prototype.isEmpty = function(){
+	return this.array.length == 0;
+};
+
+
 function dec2bin(n){
-    var binario = [];   
+    var binario = new Stack();   
     var resto = 0;
         for(var i = n; i > 0; i = Math.floor(i / 2)){
             resto = i % 2;
-            binario.unshift(resto);
+            binario.push(resto);
         }
-        return binario.join("");
+        var stringBinario = "";
+        	while (!binario.isEmpty()) {
+				stringBinario = binario.pop();
+		}
+	return stringBinario;
 }
