e7344bea74f16cc56bf41106a654e7deb977c7a1
AdrMel <melilloadriano@outlook.it>
2015-11-20 14:49:34 +0100

    Dec2Bin

--- homework_04.js
+++ homework_04.js
@@ -1,9 +1,9 @@
 function dec2bin(n){
-    var binario = [];
+    var binario = [];   
     var resto = 0;
         for(var i = n; i > 0; i = Math.floor(i / 2)){
             resto = i % 2;
             binario.unshift(resto);
         }
-     return binario.toString();
+        return binario.join("");
 }
